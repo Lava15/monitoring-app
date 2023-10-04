171cef22f57639ba356577731733378d94002194
Laziz <lazakhmediv1@gmail.com>
2023-10-04 21:25:05 +0500

    +1 test case

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature\Auth;
 use App\Http\Controllers\Auth\AuthenticationController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\Response;
 
 it('checks validation of users input', function (): void {
@@ -45,5 +46,20 @@ it('creates a new user', function (): void {
 
 });
 
+
+it('stores access token in cache', function (): void {
+
+    $response = $this->postJson(
+        uri: action([RegisterController::class, 'register']),
+        data: [
+            'name' => 'Test user',
+            'email' => 'test@example.com',
+            'password' => 'password'
+        ]
+    );
+    expect(
+        value: Cache::get($response->json())
+    )->not->toBeNull();
+});
 
 
